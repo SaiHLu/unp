a113fb731c23e2e45edfac1e1c4eab9b1429e1fe
Pyae Sone Aung <pyaesoneaung.code@gmail.com>
2020-11-27 02:20:44 +0630

    add php7.4-curl

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ apt update -y
 apt upgrade -y
 apt install nginx -y
 apt install php-fpm -y
-apt install php7.4-common php7.4-bcmath php7.4-json php7.4-mbstring php7.4-mysql php7.4-xml php7.4-zip -y
+apt install php7.4-common php7.4-bcmath php7.4-json php7.4-mbstring php7.4-mysql php7.4-xml php7.4-zip php7.4-curl -y
 apt install composer -y
 apt install nodejs -y
 apt install npm -y
