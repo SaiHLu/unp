e14ffdcaa455febd66c0073ef1030641c3ca1185
Pyae Sone Aung <pyaesoneaungrgn@gmail.com>
2021-05-25 08:33:34 +0630

    use php 8.0

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ## Script to install nginx and php on ubuntu
 | Name | Version |
 | ----------- | ----------- |
-| nginx | ^1.18 |
-| php | ^7.4 |
-| composer | ^2.0 |
-| node | ^14.15 |
-| npm | ^6.14 |
+| nginx | >=1.18 |
+| php | >=8.0 |
+| composer | >=2.0 |
+| node | >=14.15 |
+| npm | >=6.14 |
 
 ### Requirement
 ```
@@ -14,13 +14,13 @@ ubuntu 20.04
 
 ### Installation (Option 1)
 ```bash
-git clone https://github.com/PyaeSoneAungRgn/unp
-cd unp
-chmod +x install.sh
-./install.sh
+bash <(curl -s https://raw.githubusercontent.com/PyaeSoneAungRgn/unp/main/install.sh)
 ```
 
 ### Installation (Option 2)
 ```bash
-bash <(curl -s https://raw.githubusercontent.com/PyaeSoneAungRgn/unp/main/install.sh)
+git clone https://github.com/PyaeSoneAungRgn/unp
+cd unp
+chmod +x install.sh
+./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
-apt update -y
-apt upgrade -y
-apt install nginx -y
-apt install php-fpm -y
-apt install php7.4-common php7.4-bcmath php7.4-json php7.4-mbstring php7.4-mysql php7.4-xml php7.4-zip php7.4-curl -y
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt install nodejs -y
+#!/bin/bash
+
+apt -y update
+apt -y upgrade
+apt -y install nginx
+apt -y install software-properties-common
+add-apt-repository -y ppa:ondrej/php
+apt -y install php8.0-fpm
+apt -y install php8.0-common php8.0-bcmath php8.0-json php8.0-mbstring php8.0-mysql php8.0-xml php8.0-zip php8.0-curl
+curl -sL https://deb.nodesource.com/setup_14.x | -E bash -
+apt -y install nodejs
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
+php composer-setup.php --install-dir=/usr/bin --filename=composer
+cd -
