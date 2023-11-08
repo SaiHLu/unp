9c01f3ec5b8d435ca112307e861c6bb907a2c5af
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-11-08 20:57:29 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+
 apt -y update
-apt -y upgrade
+apt upgrade --assume-yes
+
+apt -y install curl
+
 apt -y install nginx
+
 apt -y install software-properties-common
 add-apt-repository -y ppa:ondrej/php
-apt -y install php8.1-fpm
-apt -y install php8.1-common php8.1-bcmath php8.1-mbstring php8.1-mysql php8.1-xml php8.1-zip php8.1-curl php8.1-gd
+apt -y install php8.2-fpm
+apt -y install php8.2-common php8.2-bcmath php8.2-mbstring php8.2-mysql php8.2-xml php8.2-zip php8.2-curl php8.2-gd
+
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --install-dir=/usr/bin --filename=composer
 cd -
+
+apt -y install mysql-server
+systemctl start mysql.service
+
+cd ~
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+apt -y install nodejs
+cd -
+
