37c3d68071cc3e216776ebcaf53f1aebe7d93209
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-12-07 02:26:24 +0630

    add php8.2-intl

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ apt -y install nginx
 apt -y install software-properties-common
 add-apt-repository -y ppa:ondrej/php
 apt -y install php8.2-fpm
-apt -y install php8.2-common php8.2-bcmath php8.2-mbstring php8.2-mysql php8.2-xml php8.2-zip php8.2-curl php8.2-gd php8.2-redis
+apt -y install php8.2-common php8.2-bcmath php8.2-mbstring php8.2-mysql php8.2-xml php8.2-zip php8.2-curl php8.2-gd php8.2-redis php8.2-intl
 
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
