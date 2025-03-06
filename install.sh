8e111017471b687a62466a61b645d284dc182b52
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2025-03-06 15:08:36 +0700

    update: php8.4

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ apt -y install nginx
 
 apt -y install software-properties-common
 add-apt-repository -y ppa:ondrej/php
-apt -y install php8.3-fpm
-apt -y install php8.3-common php8.3-bcmath php8.3-mbstring php8.3-mysql php8.3-xml php8.3-zip php8.3-curl php8.3-gd php8.3-redis php8.3-intl
+apt -y install php8.4-fpm
+apt -y install php8.4-common php8.4-bcmath php8.4-mbstring php8.4-mysql php8.4-xml php8.4-zip php8.4-curl php8.4-gd php8.4-redis php8.4-intl
 
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
